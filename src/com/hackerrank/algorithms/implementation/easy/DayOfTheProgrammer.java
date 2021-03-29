b9d03ad62a4c803761a83fb8f4166afa874809bc
Yanislav Mitev <yanislav.mitev@gmail.com>
2021-03-29 21:15:41 +0300

    Add hacker rank problems:     - Migratory Birds     - Day Of Programmer

---
+++ src/com/hackerrank/algorithms/implementation/easy/DayOfTheProgrammer.java
@@ -0,0 +1,33 @@
+package com.hackerrank.algorithms.implementation.easy;
+
+import java.util.Scanner;
+
+public class DayOfTheProgrammer {
+
+    // Complete the dayOfProgrammer function below.
+    static String dayOfProgrammer(int year) {
+        boolean isJulian = year < 1918;
+
+        boolean isLeap = isJulian ?
+                         year % 4 == 0 :
+                         year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+
+        if (year != 1918) {
+            if (isLeap) {
+                return "12.09." + year;
+            } else {
+                return "13.09" + year;
+            }
+        } else {
+            return "26.09." + year;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println(dayOfProgrammer(year));
+    }
+}
--- src/com/hackerrank/algorithms/implementation/easy/MigratoryBirds.java
+++ src/com/hackerrank/algorithms/implementation/easy/MigratoryBirds.java
@@ -3,7 +3,8 @@ package com.hackerrank.algorithms.implementation.easy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -11,8 +12,30 @@ import static java.util.stream.Collectors.toList;
 public class MigratoryBirds {
     // Complete the migratoryBirds function below.
     static int migratoryBirds(List<Integer> arr) {
+        /*  birdId,  amount **/
+        Map<Integer, Integer> countMap = new HashMap<>();
 
-        return 0;
+        for (int bird : arr) {
+            Integer amountOfBirds = countMap.get(bird);
+            if (amountOfBirds == null) {
+                countMap.put(bird, 1);
+            } else {
+                countMap.put(bird, ++amountOfBirds);
+            }
+        }
+
+        Supplier<Comparator<Map.Entry<Integer, Integer>>> compareByValue = Map.Entry::comparingByValue;
+
+        int max = countMap.entrySet().stream()
+                                     .max(compareByValue.get())
+                                     .orElseThrow(IllegalArgumentException::new)
+                                     .getValue();
+
+        return countMap.entrySet().stream()
+                                  .filter(entry -> entry.getValue() == max)
+                                  .findFirst()
+                                  .orElseThrow(IllegalArgumentException::new)
+                                  .getKey();
     }
 
     public static void main(String[] args) throws IOException {
