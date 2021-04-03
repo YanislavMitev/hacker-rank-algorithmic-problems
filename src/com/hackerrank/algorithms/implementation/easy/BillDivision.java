7495e80f2631b053811fdb71e64658af2c6f7352
Yanislav Mitev <yanislav.mitev@gmail.com>
2021-04-03 15:11:52 +0300

    Add two easy problems:     - Bill Division     - Drawing Book

---
+++ src/com/hackerrank/algorithms/implementation/easy/BillDivision.java
@@ -0,0 +1,45 @@
+package com.hackerrank.algorithms.implementation.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BillDivision {
+    // Complete the bonAppetit function below.
+    static void bonAppetit(List<Integer> bill, int annaDoesNotEat, int moneyAnnaToBeCharged) {
+        int billOfAnna = 0;
+
+        for (int foodIndex = 0; foodIndex < bill.size(); foodIndex++) {
+            int currentPrice = bill.get(foodIndex);
+
+            if (foodIndex != annaDoesNotEat) {
+                billOfAnna += currentPrice;
+            }
+        }
+
+        int overcharged = moneyAnnaToBeCharged - (billOfAnna / 2);
+        if (overcharged <= 0) {
+            System.out.println("Bon Appetit");
+        } else {
+            System.out.println(overcharged);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        int annaDoesNotEat = scanner.nextInt();
+
+        List<Integer> bill = new ArrayList<>();
+
+        for (int price = 0; price < n; price++) {
+            bill.add(scanner.nextInt());
+        }
+
+        int moneyAnnaToBeCharged = scanner.nextInt();
+
+        bonAppetit(bill, annaDoesNotEat, moneyAnnaToBeCharged);
+    }
+}
---
+++ src/com/hackerrank/algorithms/implementation/easy/DrawingBook.java
@@ -0,0 +1,44 @@
+package com.hackerrank.algorithms.implementation.easy;
+
+import java.util.Scanner;
+
+public class DrawingBook {
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int numberOfPages, int page) {
+        if (page > numberOfPages) {
+            return -1;
+        }
+
+        if (page == 0 || page == 1 || page == numberOfPages) {
+            return 0;
+        }
+
+        if (page <= numberOfPages / 2) {
+            if (page % 2 == 0) {
+                return page / 2;
+            }
+
+            return (page - 1) / 2;
+        } else {
+            if (numberOfPages % 2 == 0 && page % 2 != 0) {
+                return (int) Math.ceil((numberOfPages - page) / 2) + 1;
+            }
+
+            return (numberOfPages - page) / 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Number of pages");
+        int numberOfPages = scanner.nextInt();
+
+        System.out.println("Page");
+        int page = scanner.nextInt();
+
+        System.out.println(pageCount(numberOfPages, page));
+    }
+}
